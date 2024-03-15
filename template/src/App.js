6788fbc8c64a7fdb2b40870a005a4ea22f06ead2
Oh9yu <kog6148@gmail.com>
2024-03-15 22:23:17 +0900

    template setting

---
+++ template/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+function App() {
+  return <Container>1234</Container>;
+}
+
+export default App;
+
+const Container = styled.div``;
