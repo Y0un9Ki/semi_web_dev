ae98415e815dd3448e1b01b13010459095661d04
Y0un9Ki <srilankakim66@gmail.com>
2024-03-17 15:32:16 +0900

    Delete : backend branch

--- api/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-# Create your models here.
-class User(AbstractUser):
-    location = models.CharField(max_length=100, blank=True, null = True)
